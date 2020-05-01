2e7f9cc71a2455345420c98171223922e843fe21
Manav Misra <manavm@visionwebsoft.com>
2020-04-30 21:32:33 -0500

    :sparkles: Toggle overlay

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector('.dark')
 const img = document.querySelector('.displayed-img')
+const imgStyle = window.getComputedStyle(img)
 const overlay = document.querySelector('.overlay')
-const overlayStyles = getComputedStyle(overlay)
+const overlayStyle = window.getComputedStyle(overlay)
 const thumbnailBar = document.querySelector('.thumb-bar')
 
 for (let i = 1; i <= 5; i++) {
@@ -12,3 +13,11 @@ for (let i = 1; i <= 5; i++) {
 }
 
 /* Darken/Lighten button */
+btn.addEventListener('click', () => {
+  // EXPLICITLY CONVERT to a NUMBER for more reliable results
+  if (Number(overlayStyle.getPropertyValue('--height')) === 0) {
+    overlay.style.setProperty('--height', imgStyle.getPropertyValue('height'))
+  } else {
+    overlay.style.setProperty('--height', '0')
+  }
+})
