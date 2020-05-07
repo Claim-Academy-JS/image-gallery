38aba3fc4b3e53e28e3cc6c38a85ce86085cdf68
Manav Misra <manavm@visionwebsoft.com>
2020-05-07 13:59:17 -0500

    Update solution for image gallery
    Adding entire new file due to confusion of 'git' regarding history of different repos and branches.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector(button)
+const btn = document.querySelector('button')
 const displayedImg = document.querySelector('.displayed-img')
 const displayedImgStyle = window.getComputedStyle(displayedImg)
 const overlay = document.querySelector('.overlay')
@@ -8,21 +8,21 @@ const thumbnailBar = document.querySelector('.thumb-bar')
 // TODO: Consider adding some animation on 'src' change...😅
 
 for (let i = 1; i <= 5; i++) {
-  const imgEl = document.createElement('img')
-  imgEl.src = `./images/pic${i}.jpg`
+  const img = document.createElement('img')
+  img.src = `./images/pic${i}.jpg`
 
-  imgEl.addEventListener('click', () => {
-    // 'imgEl' is in SCOPE
-    displayedImg.src = imgEl.src
+  img.addEventListener('click', () => {
+    // 'img' is in SCOPE
+    displayedImg.src = img.src
   })
 
-  thumbnailBar.appendChild(imgEl)
+  thumbnailBar.appendChild(img)
 }
 
 /* Darken/Lighten button */
 btn.addEventListener('click', () => {
-  // 'height' uses 'px' units in browser!
-  if (overlayStyle.getPropertyValue('--height') === "0px") {
+  // 'parseInt' to avoid confusions with 'string' vs 'number' and units, etc.
+  if (parseInt(overlayStyle.getPropertyValue('--height')) === 0) {
     overlay.style.setProperty('--height', displayedImgStyle.getPropertyValue('height'))
   } else {
     overlay.style.setProperty('--height', '0')
